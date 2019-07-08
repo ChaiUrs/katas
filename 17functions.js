b370a7c355fdcfd1239c5936cb8f2f22435e63e5
ChaiUrs <mysorechaitra@gmail.com>
2019-07-08 23:12:07 +0000

    Functions and Objects challenges completed

---
+++ 17functions.js
@@ -0,0 +1,6 @@
+function eat(food)
+{
+  var food = "bananas";
+  return food + ' tasted really good.'; 
+} 
+console.log(eat("bananas"));
---
+++ 18functionarguments.js
@@ -0,0 +1,10 @@
+function math(num1, num2, num3)
+{
+  var num1 = 53;
+  var num2 = 61;
+  var num3 = 67;
+  var result1 = num2 * num3;
+  var result2 = result1 + num1;
+  return result2;
+}
+console.log(math(53, 61, 67));
