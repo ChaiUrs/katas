8f75e8c741049dc53c862fc210a66e25b67063d0
ChaiUrs <mysorechaitra@gmail.com>
2019-07-19 08:33:51 +0000

    Outputs of Katas 1-15

---
+++ katas_execution.js
@@ -0,0 +1,148 @@
+/* Execution of Katas 1-15 */
+
+vagrant [javascripting]> node kata1.js
+11
+5
+126
+
+vagrant [javascripting]> node kata2.js
+6
+9
+144
+0
+
+vagrant [javascripting]> node kata3.js
+o
+a
+e
+3
+i
+o
+u
+e
+a
+5
+A
+E
+I
+O
+U
+5
+
+vagrant [javascripting]> node kata4.js
+{ name: 'Jeremiah', course: 'Web' }
+{ name: 'Domascus', course: 'Web' }
+
+vagrant [javascripting]> node kata5.js
+Lighthouse%20Labs
+%20Lighthouse%20Labs%20
+blue%20is%20greener%20than%20purple%20for%20sure
+
+vagrant [javascripting]> node kata6.js
+1111111111
+11,222
+10101010,343434343434,9292
+
+vagrant [javascripting]> node kata7.js
+thisIsAString
+loopyLighthouse
+supercalifragalisticexpialidocious
+
+vagrant [javascripting]> node kata8.js
+
+1 
+
+
+
+1 2 3 4 5 
+
+2 4 6 8 10 
+
+3 6 9 12 15 
+
+4 8 12 16 20 
+
+5 10 15 20 25 
+
+
+
+1 2 3 4 5 6 7 8 9 10 
+
+2 4 6 8 10 12 14 16 18 20 
+
+3 6 9 12 15 18 21 24 27 30 
+
+4 8 12 16 20 24 28 32 36 40 
+
+5 10 15 20 25 30 35 40 45 50 
+
+6 12 18 24 30 36 42 48 54 60 
+
+7 14 21 28 35 42 49 56 63 70 
+
+8 16 24 32 40 48 56 64 72 80 
+
+9 18 27 36 45 54 63 72 81 90 
+
+10 20 30 40 50 60 70 80 90 100 
+
+
+vagrant [javascripting]> node kata9.js
+December 2nd, 2017
+November 11th, 2007
+August 24th, 1987
+
+vagrant [javascripting]> node kata10.js
+{ twoDollar: 1, dime: 1, penny: 3 }
+{ tenDollar: 1, twoDollar: 1, oneDollar: 1, quarter: 3, penny: 2 }
+{ twoDollar: 2, quarter: 3, dime: 2, penny: 4 }
+
+vagrant [javascripting]> node kata11.js
+{ iOS: [ 'Samuel' ], Web: [ 'Victoria', 'Karim', 'Donald' ] }
+{ Blockchain: [ 'Brendan' ],
+  Web: [ 'David', 'Carlos' ],
+  iOS: [ 'Martha' ] }
+
+vagrant [javascripting]> node kata12.js
+thisIsAString
+ThisIsAString
+this_is_a_string
+this-is-a-string
+This Is A String
+thIs Is A strIng
+THiS iS a STRiNG
+THIS_IS_A_STRING
+
+vagrant [javascripting]> node kata13.js
+{ duck: 'rubber' }
+{ bootcamp: 'Lighthouse Labs' }
+{ city: 'Vancouver', weather: 'lots of rain' }
+lots of rain
+
+vagrant [javascripting]> node kata14.js
+length_of_a_message:8
+number_of_columns:3
+clu	hlt	io	
+
+length_of_a_message:10
+number_of_columns:4
+fto	ehg	ee	dd	
+
+length_of_a_message:12
+number_of_columns:4
+hae	and	via	ecy	
+
+length_of_a_message:54
+number_of_columns:8
+imtgdvs	fearwer	mayoogo	anouuio	ntnnlvt	wttddes	aohghn	sseoau	
+
+vagrant [javascripting]> node kata15.js
+[ [ 0, 0, 0, 0, 0, 1, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 1, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+  [ 0, 0, 0, 0, 0, 0, 0, 0 ] ]
+threat_to_queen = true
